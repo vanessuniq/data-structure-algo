367f57d5d1e1beaa9e068a2081a363e00a0e25b2
vanessuniq <vanessuniq@gmail.com>
2021-01-23 00:02:48 -0500

    pseudo code solution

--- easy/longuest-common-prefix.js
+++ easy/longuest-common-prefix.js
@@ -22,3 +22,14 @@ Constraints:
 0 <= strs[i].length <= 200
 strs[i] consists of only lower-case English letters.
  */
+
+/*
+   Solution
+
+   -define an empty string variable that will save the common chars
+   -if the arrary is empry, return the empty string
+   -iterate over the char of the string with the shortest length in the array
+   -at each iteration check if the substring is the start of every string in the array
+   -if it is, set the previously define variable equal to the substring, if not continue
+  - return the varialble defined
+ */
