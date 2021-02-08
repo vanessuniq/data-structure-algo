c4b5819107a2578a8e2ea4611a5c204b7f12edc8
vanessuniq <vanessuniq@gmail.com>
2021-02-07 23:31:28 -0500

    implemented finding and deleting data from the linked list

--- LinkedLists/linkedListsImplementation.js
+++ LinkedLists/linkedListsImplementation.js
@@ -19,26 +19,30 @@ class LinkedList {
             const node = new Node(data);
             this.head = node;
             this.tail = node;
+            this.size = 1
         } else {
             this.head = null;
             this.tail = null;
+            this.size = 0
         }
         //Having access to a head and tail makes appending and prepending, getting first and last node
-        // operations constant time O(1).
+        // operations constant time O(1). Also initializing the list with the size  makes getting the length
+        // of the list a constant time operation as well
     };
-    // Checking if the list is empty
+    // Checking if the list is empty O(1)
     isEmpty() {
-            return !!this.head
+            return !!this.head;
+            // or return this.size === 0
         }
-        // Getting the first node of the list
+        // Getting the first node of the list O(1)
     getFirst() {
         return this.head;
     };
-    // Getting the last node of the list
+    // Getting the last node of the list O(1)
     getLast() {
         return this.tail;
     };
-    // Adding a node at the end of the list
+    // Adding a node at the end of the list O(1)
     append(data) {
         const node = new Node(data);
         // if the list is empty, set the head = node
@@ -49,9 +53,11 @@ class LinkedList {
             this.tail.next = node;
             this.tail = node;
         }
+        // increase the size
+        this.size += 1;
         return node;
     };
-    // Adding a node at the beginning of the list
+    // Adding a node at the beginning of the list O(1)
     preppend(data) {
         const node = new Node(data);
         // if the list is empty, set the head = node
@@ -63,7 +69,102 @@ class LinkedList {
             node.next = this.head;
             this.head = node;
         };
+        //increase the size
+        this.size += 1;
         return node
     };
+
+    // insert an element at a given position of the list O(n)
+    insertAt(data, position) {
+        // if position is greater than the size of the list, add to the end of the list
+        if (position >= this.size) {
+            this.append(data)
+                // if position is index 0, preppend the data
+        } else if (position === 0) {
+            this.preppend(data)
+        } else {
+            const node = new Node(data);
+            // start a counter
+            let start = 0;
+            // keep track of previous and current node
+            let previous = null,
+                current = this.getFirst();
+            // iterate over the list until the given position is reached
+            while (start < position) {
+                previous = current;
+                current = current.next;
+                start++
+            };
+            // attach the current node to the pointer of the insert element
+            node.next = current;
+            // set the previous node to point to the insert element
+            previous.next = node;
+
+        }
+        // increase the size
+        this.size += 1;
+    };
+
+    // find an element and return its index O(n)
+    indexOf(data) {
+        // if the list is empty return -1
+        if (this.isEmpty()) {
+            return -1
+        }
+        // initialize the counter and the current node
+        let count = 0,
+            current = this.getFirst();
+        // iterate over the list
+        while (current) {
+            // check if the current node data is equal to the given data and return the count if it is
+            if (current.data === data) {
+                return count
+            };
+            // if no match, move to the next node and increase the count
+            current = current.next;
+            count++;
+        };
+        // if the element is not found
+        return -1
+    };
+
+    // remove an element from the list O(n)
+    delete(data) {
+        // return false if the list is empty
+        if (this.isEmpty()) {
+            return false
+        };
+        // track the current and previous node
+        let previous = null,
+            current = this.getFirst();
+        // itertate over the list
+        while (current) {
+            // compare the given data to the current node data
+            if (current.data === data) {
+                if (previous) {
+                    // if the previous node is not null
+                    previous.next = current.next
+
+                    if (!current.next) {
+                        // if the current node is the last node, set the tail to the previous node as well
+                        this.tail = previous
+                    }
+
+                } else {
+                    // if the previous node is null, it means that the given data was found at the head of the list
+                    this.head = current.next
+                };
+                this.size -= 1;
+                return current;
+            };
+            // if there's no match
+            previous = current;
+            current = current.next;
+        };
+
+        // if the data not found
+        return false;
+    }
+
 
 }
