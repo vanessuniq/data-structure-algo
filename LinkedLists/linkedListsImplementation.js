11cb5776512e7c2af8d1ae93eee283557087d487
vanessuniq <vanessuniq@gmail.com>
2021-02-07 20:18:15 -0500

    defined constant access of list element at each end, check if list is empty and adding elemt to biginning or end of list

--- LinkedLists/linkedListsImplementation.js
+++ LinkedLists/linkedListsImplementation.js
@@ -23,6 +23,47 @@ class LinkedList {
             this.head = null;
             this.tail = null;
         }
+        //Having access to a head and tail makes appending and prepending, getting first and last node
+        // operations constant time O(1).
+    };
+    // Checking if the list is empty
+    isEmpty() {
+            return !!this.head
+        }
+        // Getting the first node of the list
+    getFirst() {
+        return this.head;
+    };
+    // Getting the last node of the list
+    getLast() {
+        return this.tail;
+    };
+    // Adding a node at the end of the list
+    append(data) {
+        const node = new Node(data);
+        // if the list is empty, set the head = node
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        return node;
+    };
+    // Adding a node at the beginning of the list
+    preppend(data) {
+        const node = new Node(data);
+        // if the list is empty, set the head = node
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            // point the node next property to the list head then set the list head to the node
+            node.next = this.head;
+            this.head = node;
+        };
+        return node
+    };
 
-    }
 }
